57bf6c6c9f934e88c4d10d33bdbbbadd3f24aa5e
Steve Yardumian <syardumi@gmail.com>
2017-08-24 18:03:03 -0400

    Add MIT license; add callback for when the script is finished

--- util.js
+++ util.js
@@ -1,3 +1,24 @@
+/*
+Copyright (c) 2017 Steve Yardumian
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 "use strict";
 
 var AWS = require("aws-sdk");
@@ -7,12 +28,14 @@ var fs = require("fs");
 var dateTime = require("date-time");
 var md5 = require("md5");
 
-module.exports.decompress = function(/*String*/command) {
+module.exports.decompress = function(/*String*/command, /*Function*/ cb) {
 
   if (!command.bucket || !command.file) {
     console.log("Error: missing either bucket name or full filename!");
     process.exit(1);
   }
+
+  //TODO:check that unzipped folder doesn't already exist
 
   s3.getObject(
     {
@@ -47,6 +70,11 @@ module.exports.decompress = function(/*String*/command) {
       	var zipEntries = zip.getEntries();
         var zipEntryCount = Object.keys(zipEntries).length;
 
+        if (zipEntryCount === 0){
+          cb(new Error("Error: the zip file was empty!"));
+          process.exit(1);
+        }
+
         //for each file in the zip, decompress and upload it to S3; once all are uploaded, delete the tmp zip
         var counter = 0;
       	zipEntries.forEach(function(zipEntry) {
@@ -56,6 +84,7 @@ module.exports.decompress = function(/*String*/command) {
             else console.log("File decompressed to S3: "+data.Location);
             if (zipEntryCount === counter){
               fs.unlinkSync("/tmp/"+tmpZipFilename+".zip");
+              cb(null, "Success!");
             }
           });
       	});
